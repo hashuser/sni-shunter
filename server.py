d93dcf3bfe71abdfc9f1eab6194474e3860b8f46
hashuser <60065064+hashuser@users.noreply.github.com>
2020-08-02 14:54:51 +0800

    Add files via upload

--- server.py
+++ server.py
@@ -4,33 +4,61 @@ import os
 import sys
 import json
 import traceback
+import time
 
 class core():
     def __init__(self):
         self.loop = asyncio.get_event_loop()
-        if socket.has_dualstack_ipv6():
-            listener = socket.create_server(address=('::', self.config['listen']), family=socket.AF_INET6,
-                                            dualstack_ipv6=True)
-        else:
-            listener = socket.create_server(address=('0.0.0.0', self.config['listen']), family=socket.AF_INET,
-                                            dualstack_ipv6=False)
-        server = asyncio.start_server(client_connected_cb=self.handler, sock=listener, backlog=4096)
+        listeners = []
+        servers = []
+        for addr in self.config['listen']:
+            try:
+                if '[' in addr[:addr.rfind(':')]:
+                    addr = addr.replace('[', '')
+                    addr = addr.replace(']', '')
+                    listeners.append(socket.create_server(address=(addr[:addr.rfind(':')], int(addr[addr.rfind(':') + 1:])),
+                                             family=socket.AF_INET6, dualstack_ipv6=False))
+                    listeners.append(socket.create_server(address=(addr[:addr.rfind(':')], int(addr[addr.rfind(':') + 1:])),
+                                             family=socket.AF_INET6, dualstack_ipv6=True))
+                else:
+                    listeners.append(socket.create_server(address=(addr[:addr.rfind(':')], int(addr[addr.rfind(':') + 1:])),
+                                             family=socket.AF_INET, dualstack_ipv6=False))
+            except Exception:
+                print("Invalid address", addr)
+        for listener in listeners:
+            servers.append(asyncio.start_server(client_connected_cb=self.handler, sock=listener, backlog=4096))
         self.loop.set_exception_handler(self.exception_handler)
-        self.loop.create_task(server)
+        asyncio.gather(*servers)
         self.loop.run_forever()
 
     async def handler(self, client_reader, client_writer):
         try:
+            print('New')
             client_hello = await client_reader.read(256)
-            for x in self.SNIs:
-                if x in client_hello:
-                    server_reader, server_writer = await asyncio.open_connection(host=self.SNIs[x][0],
-                                                                                 port=self.SNIs[x][1])
-                    server_writer.write(client_hello)
-                    await server_writer.drain()
-                    await asyncio.gather(self.switch(client_reader, server_writer, client_writer),
-                                         self.switch(server_reader, client_writer, server_writer))
-                    break
+            host = None
+            port = None
+            for x in self.rules:
+                if b'sni' in x[:5]:
+                    sni = x[x.find(b'=') + 1:]
+                    if ((b'!=' in x[:5] and sni not in client_hello) or ( b'!=' not in x[:5] and sni in client_hello)) and client_writer.get_extra_info("sockname")[1] in self.rules[x][0]:
+                        host = self.rules[x][1][0]
+                        port = self.rules[x][1][1]
+                        break
+                elif b'url' in x[:5]:
+                    if x.find(b'/',x.find(b'=')+8) == -1:
+                        path = b'/'
+                    else:
+                        path = x[x.find(b'/',x.find(b'=')+8):]
+                    domain = x[:x.find(b'/',x.find(b'=')+8)].replace(b'url=http://',b'')
+                    if ((b'!=' in x[:5] and (path not in client_hello or domain not in domain)) or ( b'!=' not in x[:5] and (path in client_hello and domain in client_hello))) and client_writer.get_extra_info("sockname")[1] in self.rules[x][0]:
+                        host = self.rules[x][1][0]
+                        port = self.rules[x][1][1]
+                        break
+            server_reader, server_writer = await asyncio.open_connection(host=host,port=port)
+            server_writer.write(client_hello)
+            await server_writer.drain()
+            await asyncio.gather(self.switch(client_reader, server_writer, client_writer),
+                                 self.switch(server_reader, client_writer, server_writer))
         except Exception as e:
             traceback.clear_frames(e.__traceback__)
             e.__traceback__ = None
@@ -70,7 +98,7 @@ class core():
 
 class shunter(core):
     def __init__(self):
-        self.SNIs = dict()
+        self.rules = dict()
         self.load_config()
 
     def serve_forever(self):
@@ -83,20 +111,25 @@ class shunter(core):
                 content = file.read()
             content = self.translate(content)
             self.config = json.loads(content)
-            for x in self.config['servers']:
-                if self.config['servers'][x]['sni'].lower() != 'none':
-                    self.SNIs[self.config['servers'][x]['sni'].encode('utf-8')] = (self.config['servers'][x]['ip'], self.config['servers'][x]['port'])
-            for x in self.config['servers']:
-                if self.config['servers'][x]['sni'].lower() == 'none':
-                    self.SNIs[b''] = (self.config['servers'][x]['ip'], self.config['servers'][x]['port'])
-            self.config['listen'] = int(self.config['listen'])
+            listen = set()
+            for x in self.config:
+                s_port = []
+                for y in self.config[x]['listen']:
+                    listen.add(y)
+                    s_port.append(int(y[y.rfind(':') + 1:]))
+                d_addr = self.config[x]['dst'][:self.config[x]['dst'].rfind(':')]
+                d_addr = d_addr.replace('[','')
+                d_addr = d_addr.replace(']', '')
+                d_port = int(self.config[x]['dst'][self.config[x]['dst'].rfind(':') + 1:])
+                self.rules[self.config[x]['rule'].encode('utf-8')] = (s_port,(d_addr,d_port))
+            self.config['listen'] = list(listen)
         else:
-            example = {'listen': '','servers': {'Yashmak': {'sni': '', 'ip': '', 'port': ''}}}
+            example = {'Yashmak': {'listen': [''], 'dst': '' ,'rule': ''}}
             try:
                 os.makedirs(self.config_path)
             except Exception:
                 pass
-            with open(self.config_path + 'config.json', 'w') as file:
+            with open(self.config_path + '/config.json', 'w') as file:
                 json.dump(example, file, indent=4)
 
     def translate(self, content):
